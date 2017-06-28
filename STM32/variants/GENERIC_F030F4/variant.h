7b80651ba04640d3f9fb3a9e6fe83ffc6eb9bcab
Chris <chris@chrisbarrbuilds.com>
2017-06-28 20:54:04 +0930

    F030F4 work
    Initial work to support a board built around an STM32F030F4XX @ 48MHz HSI. Working so far.

---
+++ STM32/variants/GENERIC_F030F4/variant.h
@@ -0,0 +1,15 @@
+#ifndef VARIANT_H
+#define VARIANT_H
+
+#define LED_BUILTIN PA9
+
+#define MOSI PB5
+#define MISO PB4
+#define SCK PB3
+#define SS PA4
+
+#define SDA PF0
+#define SCL PF1
+
+
+#endif
