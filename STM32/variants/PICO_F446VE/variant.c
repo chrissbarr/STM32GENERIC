daf7502f14f6e37a68980958441429109178c098
Chris <chris@chrisbarrbuilds.com>
2017-10-24 16:31:46 +1030

    F446 Changes
    Add pin-list to variant file, adding in 'imaginary' pin PB11 so that pin numbering+total is consistent for each port.
    Activate LSI clock for IWDG in Marlin.

--- STM32/variants/PICO_F446VE/variant.c
+++ STM32/variants/PICO_F446VE/variant.c
@@ -10,8 +10,9 @@ void SystemClock_Config(void) {
 
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 6;
